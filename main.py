e746f6421b7b82b30e69874172424b6d67b83f06
Sergei Konovalov <l0kifs91@gmail.com>
2024-08-05 19:31:33 +0600

    init

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI, Request
+from collections import deque
+from typing import List
+
+app = FastAPI()
+webhooks = deque(maxlen=50)
+
+
+@app.post("/webhook")
+async def receive_webhook(request: Request):
+    data = await request.json()
+    webhooks.append(data)
+    return {"message": "Webhook received"}
+
+
+@app.get("/webhooks")
+async def get_webhooks() -> List[dict]:
+    return list(webhooks)
